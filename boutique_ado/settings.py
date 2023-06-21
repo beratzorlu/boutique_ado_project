a6b4d7ea5b6599a265017b6225ee1290cda9fbe3
berat.zorlu <berat.zorlu@hotmail.com>
2023-06-21 22:32:47 +0000

    Add home page app and urls.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',  # all basic user account actions
     'allauth.socialaccount',  # handles loggin in via sm providers
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
