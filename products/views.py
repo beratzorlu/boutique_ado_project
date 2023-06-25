0a3e70b1ecbd488c83e5801b1fa26d706de365ea
berat.zorlu <berat.zorlu@hotmail.com>
2023-06-25 18:06:50 +0000

    Add sort box functionality and back to top btn.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q  # is used to generate a search query
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
