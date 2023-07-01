ba86499a60f4542ea653c5e72553b3668b3072ba
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-01 13:44:10 +0000

    Add checkout success page views and url.

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,29 +2,27 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
-class OrderLineItemAdminInLine(admin.TabularInline):
+class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
-    readlonly_fields = ('lineitem_total',)
+    readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineItemAdminInLine, )
+    inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total')
+                       'grand_total',)
 
-    fields = ('order_number', 'date',
-              'full_name', 'email', 'phone_number',
-              'country', 'postcode',
-              'town_or_city', 'street_address1',
-              'street_address2', 'county',
-              'delivery_cost', 'order_total',
-              'grand_total')
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
 
-    list_display = ('order_number', 'date',
-                    'full_name', 'order_total',
-                    'delivery_cost', 'grand_total')
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
 
     ordering = ('-date',)
 
