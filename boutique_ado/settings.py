55ea840f52f3595a9a185b7ac3386a70e8153772
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-04 22:07:09 +0000

    Add Heroku deployment config settings.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -30,7 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-beratzorlu-boutiqueadop-kprqpbui0z7.ws-eu100.gitpod.io',
-    '8000-beratzorlu-boutiqueadop-kprqpbui0z7.ws-eu101.gitpod.io'
+    '8000-beratzorlu-boutiqueadop-kprqpbui0z7.ws-eu101.gitpod.io',
+    'boutique-ado-berat.herokuapp.com',
+    'localhost',
 ]
 
 
@@ -123,13 +126,17 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
