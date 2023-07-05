c246c6600005ba8f94dc58851f01f5f6b4fee860
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-05 15:45:47 +0000

    Add cache control settings.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,6 +182,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2069 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Configs
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-beart'
     AWS_S3_REGION_NAME = 'eu-west-1'
